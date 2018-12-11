797e8059964ed3ccef0d9e5933dd0515a934ff5f
captainangus <edward.w.cook@gmail.com>
2018-12-11 00:59:41 -0500

    create picture functionality

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Picture = mongoose.model('Picture')
+
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
@@ -7,6 +10,13 @@ exports.addPicture = (req, res) => {
     res.render('editPicture', { title: 'Add Picture' });
 };
 
-exports.createPicture = (req, res) => {
-    res.json(req.body);
+exports.createPicture = async (req, res) => {
+    const picture = await (new Picture(req.body)).save();
+    req.flash('success', `Successfully Created ${picture.name}.`)
+    res.redirect(`/picture/${picture.slug}`);
+};
+
+exports.getPictures = async (req, res) => {
+    const pictures = await Picture.find();
+    res.render('pictures', { title: 'Pictures', pictures });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const pictureController = require("../controllers/pictureController");
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get("/", pictureController.homePage);
-router.get("/add", pictureController.addPicture);
-// router.post('/add', pictureController.addPicture;
+router.get('/', pictureController.homePage);
+router.get('/pictures', catchErrors(pictureController.getPictures));
+router.get('/add', pictureController.addPicture);
+router.post('/add', catchErrors(pictureController.createPicture));
 
 module.exports = router;
