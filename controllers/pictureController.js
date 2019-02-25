17f8b319ab35c800d7c8a77e19ddfc4c4549681b
captainangus <edward.w.cook@gmail.com>
2019-02-24 23:00:11 -0500

    fixed post

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -5,60 +5,85 @@ const jimp = require('jimp');
 const uuid = require('uuid');
 
 const multerOptions = {
-    storage: multer.memoryStorage(),
-    fileFilter(req, file, next) {
-        const isPhoto = file.mimetype.startsWith('image/');
-        if(isPhoto) {
-            next(null, true);
-        } else {
-            next({ message: 'That filetype isn\'t allowed!' }, false);
-        }
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
     }
-}
+  }
+};
 
 exports.homePage = (req, res) => {
-    console.log(req.name);
-    res.render('index');
+  res.render('index');
 };
 
 exports.addPicture = (req, res) => {
-    res.render('editPicture', { title: 'Add Picture' });
+  res.render('editPicture', { title: 'Add Picture' });
 };
 
 exports.upload = multer(multerOptions).single('photo');
 
 exports.resize = async (req, res, next) => {
-    //check if there is no new file to resize
-    if(!req.file) {
-        next(); //skip to next middleware
-        return;
-    }
-    const extension = req.file.mimetype.split('/')[1];
-    req.body.photo = `${uuid.v4()}.${extension}`;
-    const photo = await jimp.read(req.file.buffer);
-    await photo.resize(800, jimp.AUTO);
-    await photo.write(`./public/uploads/${req.body.photo}`);
-    next();
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going!
+  next();
 };
 
 exports.createPicture = async (req, res) => {
-    const picture = await (new Picture(req.body)).save();
-    req.flash('success', `Successfully Created ${picture.name}.`)
-    res.redirect(`/picture/${picture.slug}`);
+  const picture = await (new Picture(req.body)).save();
+  req.flash('success', `Successfully Created ${picture.name}. Care to leave a review?`);
+  res.redirect(`/picture/${picture.slug}`);
 };
 
 exports.getPictures = async (req, res) => {
-    const pictures = await Picture.find();
-    res.render('pictures', { title: 'Pictures', pictures });
+  // 1. Query the database for a list of all pictures
+  const pictures = await Picture.find();
+  res.render('pictures', { title: 'Pictures', pictures });
 };
 
 exports.editPicture = async (req, res) => {
-    const picture = await Picture.findOne({ _id: req.params.id });
-    res.render('editPicture', { title: `Edit ${picture.name}`, picture })
+  // 1. Find the picture given the ID
+  const picture = await Picture.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the picture
+  // TODO
+  // 3. Render out the edit form so the user can update their picture
+  res.render('editPicture', { title: `Edit ${picture.name}`, picture });
+};
+
+exports.updatePicture = async (req, res) => {
+  // set the location data to be a point
+  req.body.location.type = 'Point';
+  // find and update the picture
+  const picture = await Picture.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new picture instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${picture.name}</strong>. <a href="/pictures/${picture.slug}">View Picture →</a>`);
+  res.redirect(`/pictures/${picture._id}/edit`);
+  // Redriect them the picture and tell them it worked
 };
 
 exports.getPictureBySlug = async (req, res, next) => {
-    const picture = await Picture.findOne({ slug: req.params.slug });
-    if (!picture) return next();
-    res.render('picture', { picture, title: picture.name });
+  const picture = await Picture.findOne({ slug: req.params.slug });
+  if (!picture) return next();
+  res.render('picture', { picture, title: picture.name });
+};
+
+exports.getPicturesByTag = async (req, res) => {
+  const tags = await Picture.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', { tags, title: 'Tags', tag });
 };
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,28 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const pictureController = require("../controllers/pictureController");
+const pictureController = require('../controllers/pictureController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-// Do work here
 router.get('/', catchErrors(pictureController.getPictures));
-
 router.get('/pictures', catchErrors(pictureController.getPictures));
-
 router.get('/add', pictureController.addPicture);
 
-router.post('/add/:id', 
+router.post('/add',
     pictureController.upload,
     catchErrors(pictureController.resize),
-    catchErrors(pictureController.updateStore)
+    catchErrors(pictureController.createPicture)
 );
 
-router.post('/add/:id', pictureController.upload, catchErrors(pictureController.resize), catchErrors(pictureController.createPicture));
+router.post('/add/:id',
+    pictureController.upload,
+    catchErrors(pictureController.resize),
+    catchErrors(pictureController.updatePicture)
+);
 
 router.get('/pictures/:id/edit', catchErrors(pictureController.editPicture));
-
 router.get('/picture/:slug', catchErrors(pictureController.getPictureBySlug));
+
+router.get('/tags', catchErrors(pictureController.getPicturesByTag));
+router.get('/tags/:tag', catchErrors(pictureController.getPicturesByTag));
 
 module.exports = router;
